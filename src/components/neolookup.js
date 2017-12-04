61619c3539798cdb44f759d384f36d2ea12d05bc
w3bprinz <patr.koehler@gmail.com>
2017-12-04 21:00:29 +0100

    InitialSetup

---
+++ src/components/neolookup.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+class NeoLookUp  extends Component {
+    state = {  }
+    render() {
+        return (
+            
+        );
+    }
+}
+
+export default NeoLookUp ;
